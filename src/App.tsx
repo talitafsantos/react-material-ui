10beb04cda666c8b39be1a1360e59182aab1f7a2
Talita Santos <talita.santos@ipdec.org>
2023-04-14 11:10:14 -0400

    feat: instalando Material UI V5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+
 import { AppRoutes } from "./routes";
+
+
 export const App = () => {
-  return (
+  return ( 
     <BrowserRouter>
-     <AppRoutes />
+      <AppRoutes />
     </BrowserRouter>
   );
 }
